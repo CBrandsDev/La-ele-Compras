aca923bdd3da59a38747decedc31d632996c92c6
CBrandsDev <cbrandao2001@gmail.com>
2023-08-30 18:40:27 -0300

    some changes

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ const catalogo = [
 ];
 
 for (const produtoCatalogo of catalogo) {
-  const cartaoProduto = `<div class="w-48" id="card-produto-${produtoCatalogo.id}">
+  const cartaoProduto = `<div class="border-solid border-2 border-sky-500 w-48 m-2" id="card-produto-${produtoCatalogo.id}">
     <img src="./assets/img/${produtoCatalogo.imagem}.jpg" alt="Produto 1">
     <p class="marca">${produtoCatalogo.marca}</p>
     <p>${produtoCatalogo.nome}</p>
